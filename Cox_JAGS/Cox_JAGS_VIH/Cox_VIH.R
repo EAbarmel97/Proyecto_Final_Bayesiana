331def946c7a6c233a7a809dfdd85f38882ccdb7
Enki Alonso Barra M <enki.barmel97@ciencias.unam.mx>
2022-06-06 21:45:50 -0500

    agregamos un archivo en Proy_VIH

--- Cox_JAGS/Cox_JAGS_VIH/Cox_VIH.R
+++ Cox_JAGS/Cox_JAGS_VIH/Cox_VIH.R
@@ -4,8 +4,18 @@ library(coda)
 library(magrittr)
 
 #Cargamos la base ya con las variables mudas incorporadas
-
 DF_VIH <- read.csv("~/Documents/Modelo_Cox_Bayesiano/Cox_JAGS/Cox_JAGS_VIH/base_oficial_dummies.csv")
+
+#Dado que son demasiadas observaciones tomamos una muestra de 30%
+DF_VIH <- DF_VIH %>% sample_frac(0.30)
+
+#creamos una variable llamada pais
+DF_VIH$pais <- substr(DF_VIH$patient,1,2)
+
+#agrupamos por pais y tomamos muestra de un 
+DF_VIH<- DF_VIH %>%  group_by(pais) %>% sample_frac(0.3)
+
+count(DF_VIH, pais)
 
 # Matriz diseño 
 X <- DF_VIH %>% select(male, mode_Bisexual:art_grupo_primer_3)
@@ -18,12 +28,12 @@ delta = DF_VIH$death_y
 
 ######################## JAGS ###################################
 K <- length(colnames(X)) #número de intervalo que escogemos 
-a <- seq(min(time)-0.001, max(time) + 0.001, length.out = K + 1) #vector de tiempos de censura 
+a <- seq(min(time), max(time) + 0.01, length.out = K + 1) #vector de tiempos de censura 
 int.obs <- matrix(data = NA, nrow = nrow(X), ncol = length(a) - 1)
 D <- matrix(data = NA, nrow = nrow(X), ncol = length(a) - 1) 
 for(i in 1:nrow(X)){
   for (k in 1:(length(a) - 1)){
-    D[i, k] <- ifelse(time[i] - a[k] > 0, 1, 0) * ifelse(a[k + 1] - time[i] >= 0, 1, 0)#indicadora del intervalo
+    D[i, k] <- ifelse(time[i] - a[k] >= 0, 1, 0) * ifelse(a[k + 1] - time[i] > 0, 1, 0)#indicadora del intervalo
     int.obs[i, k] <- D[i, k] * k #indica en que intervalo cae la observación. 
   }
 }
@@ -45,6 +55,36 @@ param.jags <- c("beta", "lambda")  #paramteros a monitorear
 Modelo_compilado <- jags.model(data = data.jags, file = "/Users/enki/Documents/Modelo_Cox_Bayesiano/Cox_JAGS/Cox_JAGS_VIH/m_VIH.txt", 
                                inits = init.jags, n.chains = 3)
 
+#Mandamos llamar coda para tomar muestras para dist a posteriori 
+update(Modelo_compilado, 1000)
+res <- coda.samples(Modelo_compilado,variable.names=param.jags,n.iter=50000, n.thin=10)
 
+#Unimos las 3 cadenas MCMC para hacer inferencia sobre los resultados de la simulación de 
+#las dist posteriores 
+results <- as.mcmc(do.call(rbind,res))
 
+#Asiganamos a las entradas de results a beta y lambda 
+lambda_names <- c()#vector para los hiperparams lambda 
+beta_names <- c()#vector para los hiperparams beta 
+for(i in 1:34){
+  beta_names[i] = paste("beta",as.character(i), sep="")
+  lambda_names[i] = paste("lambda",as.character(i), sep="")
+}
+
+#asignación segun los componentes de results 
+for(i in 1:64){
+  if(i <= 34){
+    assign(beta_names[i], results[,i]) # los primeros 34 son los coefs de regresión 
+  }else{
+    assign(lambda_names[i], results[,i]) #los últimos 34 son los parámetros de la fun 
+    #de riesgo base
+  }
+}
+
+#Trazas 
+par(mfrow=c(2,4))
+traceplot(results)
+
+#Info sobre las dist posteriores
+summary(results)
 
